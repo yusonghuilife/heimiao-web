7fc3842ff5ea2b24c36db3de18e9d7bb33ea3678
yusonghu <yusong1209@gmail.com>
2024-11-09 10:45:05 +0800

    feat: 黑喵web布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
+import mainBgImg from './asset/gril_background.jpg'
+// import logo from './logo.svg';
 import './App.css';
+import { Flex, Layout } from 'antd';
+
+const { Header, Footer, Sider, Content } = Layout;
+
+const clsPrefix = 'heimiao'
+
+const headerStyle: React.CSSProperties = {
+  textAlign: 'center',
+  color: '#fff',
+  height: 64,
+  paddingInline: 48,
+  lineHeight: '64px',
+  backgroundColor: 'transparent',
+};
+
+const contentStyle: React.CSSProperties = {
+  textAlign: 'center',
+  minHeight: 120,
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: 'transparent',
+};
+
+const siderStyle: React.CSSProperties = {
+  textAlign: 'center',
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: 'rgba(255, 255, 255, 0.4)',
+};
+
+const footerStyle: React.CSSProperties = {
+  textAlign: 'center',
+  color: '#fff',
+  backgroundColor: 'transparent',
+};
+
+const layoutStyle: React.CSSProperties = {
+  borderRadius: 8,
+  overflow: 'hidden',
+  width: '100vw',
+  height: '100vh',
+  backgroundImage: `url(${mainBgImg})`,
+  backgroundPosition: 'center center',
+  backgroundRepeat: 'no-repeat', /* 防止图片重复 */
+  backgroundSize: 'auto',
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex gap="middle" wrap>
+      <Layout className={`${clsPrefix}-layout`} style={layoutStyle}>
+        <Header className={`${clsPrefix}-header`} style={headerStyle}>Banner</Header>
+        <Layout className={`${clsPrefix}-container`}>
+          <Content className={`${clsPrefix}-server-list`}style={contentStyle}>Content</Content>
+          <Sider className={`${clsPrefix}-rank-list`} width="25%" style={siderStyle}>
+            Sider
+          </Sider>
+        </Layout>
+        <Footer className={`${clsPrefix}-footer`} style={footerStyle}>Footer</Footer>
+      </Layout>
+    </Flex>
   );
 }
 
